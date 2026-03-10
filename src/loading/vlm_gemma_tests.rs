eb8b8aea1269427af9fc16785e6a0e85136e7cfe
Jeongkyu Shin <jshin@lablup.com>
2026-03-10 09:14:29 +0900

    Restore Apache headers across owned sources

--- src/loading/vlm_gemma_tests.rs
+++ src/loading/vlm_gemma_tests.rs
@@ -1,3 +1,17 @@
+// Copyright 2025-2026 Lablup Inc. and Jeongkyu Shin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use super::{
     gemma3n_language_model_prefix, gemma3n_metadata, gemma3n_needs_conv_transpose,
     sanitize_gemma3n_weights,
